293bcebf7c1412f9f5114ab1ddd7d2d6c5afdc18
Haytham Mohamed <haybu@hotmail.com>
2018-11-14 18:55:39 -0600

    secure flights service

--- flights-service/src/main/java/io/agilehandy/flights/config/OAuth2ResourceServerSecurityConfiguration.java
+++ flights-service/src/main/java/io/agilehandy/flights/config/OAuth2ResourceServerSecurityConfiguration.java
@@ -1,23 +1,28 @@
 package io.agilehandy.flights.config;
 
-//@Configuration
-//@EnableWebFluxSecurity
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
 public class OAuth2ResourceServerSecurityConfiguration {
 
     // @formatter:on
-	/**
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
                 .authorizeExchange()
                 .anyExchange().authenticated()
                 .and()
+		        .csrf().disable()
                 .oauth2ResourceServer()
                 .jwt()
                 ;
         return http.build();
     }
-    */
     // @formatter:off
 
 }
