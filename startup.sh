ebb8f9ef1147e541a9673cf52d777d07244f4dcf
Haytham Mohamed <haybu@hotmail.com>
2019-01-14 12:41:49 -0600

    update

--- startup.sh
+++ startup.sh
@@ -2,30 +2,42 @@
 
 LOCAL_DIR=$(pwd)
 
-mvn clean install -DskipTests
+#mvn clean install -DskipTests
 
 java -jar $LOCAL_DIR/registry-server/target/registry-server-0.0.1-SNAPSHOT.jar &
 
-sleep 6
+sleep 10
 
-java -jar $LOCAL_DIR/config-server/target/config-server-0.0.1-SNAPSHOT.jar &
-
-sleep 6
+echo "Registry service is started successfully!"
 
 java -jar $LOCAL_DIR/flights-service/target/flights-service-0.0.1-SNAPSHOT.jar &
 
-sleep 6
+sleep 10
+
+echo "Flights service is started successfully!"
 
 java -jar $LOCAL_DIR/reservations-service/target/reservations-service-0.0.1-SNAPSHOT.jar &
 
-sleep 6
+sleep 10
+
+echo "Reservations service is started successfully!"
 
 java -jar $LOCAL_DIR/agency-web/target/agency-web-0.0.1-SNAPSHOT.jar &
 
-sleep 6
+sleep 10
+
+echo "Web client is started successfully!"
 
 java -jar $LOCAL_DIR/agency-gateway/target/agency-gateway-0.0.1-SNAPSHOT.jar &
 
-sleep 6
+sleep 10
+
+echo "Gateway is started successfully!"
+
+$LOCAL_DIR/uaa-server/gradlew -b $LOCAL_DIR/uaa-server/build.gradle uaa &
+
+sleep 10
+
+echo "UAA is started successfully!"
 
 echo "All applications started successfully!"
